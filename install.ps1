15aadfd0586dec21fb56c3d87ef81d53992fc1d1
Mateusz Pietrzak <mateusz@mzpk.dev>
2026-03-22 14:58:49 +0100

    refactor(structure): promote blackwall to root level

--- install.ps1
+++ install.ps1
@@ -16,4 +16,4 @@ if (Test-Path $TARGET) {
 git clone --depth 1 --quiet $REPO $TARGET
 
 Set-Location $TARGET
-python3 ripperdoc/null/sys/scripts/install.py @args
+python3 blackwall/sys/scripts/install.py @args
